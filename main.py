855da11bdfa06be934445693c37d8860ed359774
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-08-04 13:50:13 +0300

    Created GET, POST and DELETE endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -7,56 +7,98 @@ app = Flask(__name__)
 @app.route("/o-level", methods=["GET"])
 def get_o_level_scripts():
     if request.method == "GET":
-        file = open("o-level.json")
-        data = json.load(file)
-        file.close()
-        return data
+        with open("o-level.json", "r") as file:
+            data = json.load(file)
+        return jsonify(data)
 
 
 @app.route("/o-level/add-file", methods=["POST"])
 def post_o_level_scripts():
-    file = open("o-level.json", "+w")
-    data = json.load(file)
-    new_data = data.append(request.get_json())
-    json.dump(new_data, file)
-    file.close()
-    print("Successfully added data!")
-    return "", 204
+    try:
+        with open("o-level.json", "r+") as file:
+            data = json.load(file)
+            new_data = request.get_json()
+            data.append(new_data)
+            file.seek(0)
+            json.dump(data, file)
+            file.truncate()
+            print("Successfully added data!")
+            return "", 204
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Error occurred while adding data.", 500
 
 
-@app.route("/o-level/delete", methods=["POST"])
+@app.route("/o-level/delete", methods=["DELETE"])
 def delete_o_level_scripts():
-    file = open("o-level.json", "+w")
-    data: list = json.load(file)
-    new_data = list(
-        filter(
-            lambda val: val["random_code"] != request.get_json()["random_code"], data
-        )
-    )
-    json.dump(new_data, file)
-    file.close()
-    print("Successfully deleted data!")
-    return "", 204
+    try:
+        with open("o-level.json", "r+") as file:
+            data = json.load(file)
+            request_data = request.get_json()
+            new_data = list(
+                filter(
+                    lambda val: val["random_code"] != request_data["random_code"], data
+                )
+            )
+            if len(new_data) == len(data):
+                print(f"Error: file not found!")
+                return "Error occurred while deleting data.", 404
+            file.seek(0)  # Move the file pointer to the beginning
+            json.dump(new_data, file)
+            file.truncate()  # Truncate the file to remove any extra content
+            print("Successfully deleted data!")
+            return "", 204
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Error occurred while deleting data.", 500
 
 
 @app.route("/a-level", methods=["GET"])
 def get_a_level_scripts():
-    if request.method == "GET":
-        file = open("a-level.json")
+    with open("o-level.json", "r") as file:
         data = json.load(file)
-        file.close()
-        return data
+        return jsonify(data)
 
 
 @app.route("/a-level/add-file", methods=["POST"])
 def post_a_level_scripts():
-    file = open("a-level.json", "a")
-    data: list = json.load(file)
-    new_data = data.append(request.get_json())
-    json.dump(new_data, file)
-    file.close()
-    print("Successfully added data!")
-    return "", 204
+    try:
+        with open("a-level.json", "r+") as file:
+            data = json.load(file)
+            new_data = request.get_json()
+            data.append(new_data)
+            file.seek(0)  # Move the file pointer to the beginning
+            json.dump(data, file)
+            file.truncate()  # Truncate the file to remove any extra content
+            print("Successfully added data!")
+            return "", 204
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Error occurred while adding data.", 500
+
+
+@app.route("/a-level/delete", methods=["DELETE"])
+def delete_a_level_scripts():
+    try:
+        with open("a-level.json", "r+") as file:
+            data = json.load(file)
+            request_data = request.get_json()
+            new_data = list(
+                filter(
+                    lambda val: val["random_code"] != request_data["random_code"], data
+                )
+            )
+            if len(new_data) == len(data):
+                print(f"Error: file not found!")
+                return "Error occurred while deleting data.", 404
+            file.seek(0)  # Move the file pointer to the beginning
+            json.dump(new_data, file)
+            file.truncate()  # Truncate the file to remove any extra content
+            print("Successfully deleted data!")
+            return "", 204
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Error occurred while deleting data.", 500
 
 
 if __name__ == "__main__":
